f528e804fb19dda92b90670734734fe18ba749b0
mohammadrezaaghaei1998 <136630602+mohammadrezaaghaei1998@users.noreply.github.com>
2024-11-05 19:07:08 +0400

    Update settings.py

--- vividvision/settings.py
+++ vividvision/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-0=eho48%9b*a8(7q$6*$ujywe*m1@y6j@76u%dm&h6%o@8-g&7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'FALSE') == 'True'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'www.vividvisiongcc.com').split(',')
+ALLOWED_HOSTS = ['octopus-app-eecg3.ondigitalocean.app']
+
 
 
 # Application definition
